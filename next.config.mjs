42dc5c51a815d4cc2acd8fc436976ddd32416d43
adarshrajan11 <rajadarshpk@gmail.com>
2024-11-30 23:13:45 +0530

    deployment attempts

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export", // Enables static export
+  assetPrefix: isProd ? "/myportfolio/" : "", // Prefix assets for GitHub Pages
+  basePath: isProd ? "/myportfolio" : "", // Set base path for GitHub Pages
+};
 
 export default nextConfig;
